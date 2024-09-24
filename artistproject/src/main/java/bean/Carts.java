2c9378e8b1c561e1ff164bda68fbd09c5d03abea
YangYang890120 <ja24082010@gmail.com>
2024-09-24 22:04:35 +0800

    新增CartsDao,Impl

--- artistproject/src/main/java/bean/Carts.java
+++ artistproject/src/main/java/bean/Carts.java
@@ -1,7 +1,7 @@
 package bean;
 
 public class Carts {
-
+	private String cartId;
 	private String customerId;
 	private String paintingId;
 
@@ -9,6 +9,13 @@ public class Carts {
 	}
 
 	public Carts(String customerId, String paintingId) {
+		this.customerId = customerId;
+		this.paintingId = paintingId;
+	}
+
+	public Carts(String cartId, String customerId, String paintingId) {
+		super();
+		this.cartId = cartId;
 		this.customerId = customerId;
 		this.paintingId = paintingId;
 	}
@@ -28,4 +35,13 @@ public class Carts {
 	public void setPaintingId(String paintingId) {
 		this.paintingId = paintingId;
 	}
+
+	public String getCartId() {
+		return cartId;
+	}
+
+	public void setCartId(String cartId) {
+		this.cartId = cartId;
+	}
+
 }
---
+++ artistproject/src/main/java/dao/impl/CartsDaoImpl.java
@@ -0,0 +1,107 @@
+package dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.Carts;
+import dao.CartsDAO;
+import dao.DbConnection;
+import ulitilies.IdGenerator;
+
+public class CartsDaoImpl implements CartsDAO{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	Connection conn = DbConnection.getDb();
+	@Override
+	public void create(Carts c) {
+		String SQL = "INSERT INTO artistproject.carts(carts_id,customer_id,painting_id) VALUES ("+ IdGenerator.cartsId() +",?,?";
+		try {
+			conn.setAutoCommit(false);
+			PreparedStatement ps = conn.prepareStatement(SQL);
+			ps.setString(1,c.getCustomerId());
+			ps.setString(2,c.getPaintingId());
+			ps.executeUpdate();
+			conn.commit();
+			conn.setAutoCommit(true);
+		} catch (SQLException e) {
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public List<Carts> selectAll() {
+		String SQL="select * from artistproject.carts";
+		List<Carts> data=new ArrayList<Carts>();
+		try {
+			PreparedStatement ps = conn.prepareStatement(SQL);
+			ResultSet rs=ps.executeQuery();
+			while(rs.next())
+			{
+				Carts c=new Carts();
+				c.setCartId(rs.getString("cart_id"));
+				c.setCustomerId(rs.getString("customer_id"));
+				c.setPaintingId(rs.getString("painting_id"));
+				data.add(c);
+			}
+			return data;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public void update(String cart_id,String painting_id) {
+		String SQL = "update artistproject.carts set painting_id=? where cart_id=?";
+		try {
+			conn.setAutoCommit(false);
+			PreparedStatement ps = conn.prepareStatement(SQL);
+			ps.setString(1,cart_id);
+			ps.setString(2,painting_id);
+			ps.executeUpdate();
+			conn.commit();
+			conn.setAutoCommit(true);
+		} catch (SQLException e) {
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void delete(String id) {
+		String SQL="delete from artistproject.carts where cart_id=?";
+		try {
+			conn.setAutoCommit(false);
+			PreparedStatement ps = conn.prepareStatement(SQL);
+			ps.setString(1,id);
+			ps.executeUpdate();
+			conn.commit();
+			conn.setAutoCommit(true);
+		} catch (SQLException e) {
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+	}
+
+}
