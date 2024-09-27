4c8e6c23a3ca43464f52e58cfc132a5b028a0cad
EvanceK <155881420+EvanceK@users.noreply.github.com>
2024-09-27 14:06:07 +0800

    update maven pom ver.

--- artistproject/src/main/java/controller/PaintingsController.java
+++ artistproject/src/main/java/controller/PaintingsController.java
@@ -3,6 +3,7 @@ package controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ import bean.Paintings;
 import service.impl.PaintingsServiceImpl;
 
 //@RestController 是 @Controller 和 @ResponseBody 的結合體
-
+@CrossOrigin
 @RestController
 @RequestMapping("/PTController")
 public class PaintingsController {
