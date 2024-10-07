9bb1ace6bc658ce1765c0b24e5c0077bbf4817c6
EvanceK <155881420+EvanceK@users.noreply.github.com>
2024-10-07 21:51:10 +0800

    pom update

--- ArtistProjectSpringBoot/src/main/java/com/artist/controller/CustomersController.java
+++ ArtistProjectSpringBoot/src/main/java/com/artist/controller/CustomersController.java
@@ -6,11 +6,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.artist.dto.CustomersDTO;
 import com.artist.dto.LoginResponse;
-
 import com.artist.entity.Customers;
 import com.artist.service.impl.CustomersServiceImpl;
 
@@ -20,7 +20,7 @@ public class CustomersController {
     @Autowired
     private CustomersServiceImpl csi;
 
-    //註冊
+    // 註冊
     @PostMapping(value ="/register", consumes = "application/json")
     public ResponseEntity<?> createCustomer(@RequestBody CustomersDTO customersDTO) {
     	csi.create(customersDTO);
@@ -31,12 +31,21 @@ public class CustomersController {
     @PostMapping(value ="/login", consumes = "application/json")
     public ResponseEntity<?> login(@RequestBody CustomersDTO customersDTO) {
         String token = csi.login(customersDTO.getEmail(), customersDTO.getPassword());
-		Customers customer = csi.getCustomer(customersDTO.getEmail());
-        String nickName = 	customer.getNickName();
-        System.out.println(nickName);
+		String customerId = csi.getCustomerIdFromToken(token);
+		Customers customer = csi.getByCustomerId(customerId);
+        String nickName = customer.getNickName();
         LoginResponse response = new LoginResponse(token, nickName);
         return ResponseEntity.ok((response));
     }
-    
+    // 刷新 token
+    @PostMapping("/token/refresh")
+    public ResponseEntity<?> refreshToken(@RequestParam String token) {
+        String newToken = csi.refreshToken(token);
+        if (newToken != null) {
+            return ResponseEntity.ok(newToken);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid or expired token.");
+        }
+    }
     
 }
