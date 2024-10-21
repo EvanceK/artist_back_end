d6cf083ec4ef24a730373cdaea968957ecc0ace7
Danny Tan <129268549+DannyTan8x@users.noreply.github.com>
2024-10-21 16:34:08 +0800

    update staff login

--- ArtistProjectSpringBoot/src/main/java/com/artist/service/impl/StaffServiceImpl.java
+++ ArtistProjectSpringBoot/src/main/java/com/artist/service/impl/StaffServiceImpl.java
@@ -1,28 +1,45 @@
 package com.artist.service.impl;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.artist.dto.response.StaffDTO;
 import com.artist.entity.Staff;
 import com.artist.repository.StaffRepository;
 import com.artist.service.StaffService;
+import com.artist.utils.JwtUtil;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Service
-public class StaffServiceImpl implements StaffService{
+public class StaffServiceImpl implements StaffService {
 
 	@Autowired
 	private StaffRepository sr;
-	
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+
 	@Override
 	public void create(StaffDTO staffDTO) {
-		Staff staff = new Staff(staffDTO.getStaffName(),
-				staffDTO.getStaffDepartment(),
-				staffDTO.getStaffUsername(),
-				staffDTO.getStaffPassword());		
+		Staff staff = new Staff(staffDTO.getStaffName(), staffDTO.getStaffDepartment(), staffDTO.getStaffUsername(),
+				passwordEncoder.encode(staffDTO.getStaffPassword()), staffDTO.getRoleId());
 		sr.save(staff);
 	}
 
@@ -47,16 +64,55 @@ public class StaffServiceImpl implements StaffService{
 			staff.setStaffName(staffDTO.getStaffName());
 			staff.setStaffUsername(staffDTO.getStaffUsername());
 			staff.setStaffPassword(staffDTO.getStaffPassword());
+			staff.setRoleId(staffDTO.getRoleId());
 			sr.save(staff);
-		}else {
+		} else {
 			System.out.println("data not fond");
 		}
-		
+
 	}
 
 	@Override
 	public void deleteByStaffId(Integer staffId) {
 		sr.deleteById(staffId);
+	}
+
+	@Override
+	public String login(String staffUsername, String staffPassword) {
+		// TODO Auto-generated method stub
+		Staff staff = sr.findByStaffUsername(staffUsername)
+				.orElseThrow(() -> new RuntimeException("Staff UserName doesn't exist"));
+		if (passwordEncoder.matches(staffPassword, staff.getStaffPassword())) {
+			return generateToken(staff);
+		} else {
+			throw new RuntimeException("Invalid password");
+		}
+
+	}
+
+	private String generateToken(Staff staff) {
+		Map<String, Object> claims = new HashMap<>();
+		claims.put("StaffId", staff.getStaffId());
+		claims.put("roleId", staff.getRoleId());
+		System.out.println("staffID"+staff.getStaffId());
+		return Jwts.builder().setSubject(staff.getStaffUsername()).addClaims(claims)
+				.setExpiration(new Date(System.currentTimeMillis() + 86400000))
+				.signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+
+	}
+
+	public Integer getStaffIdFromToken(String token) {
+		if (token.startsWith("Bearer ")) {
+			token = token.substring(7);
+		}
+
+		Claims claims = Jwts.parser()
+				.setSigningKey(jwtSecret)
+				.parseClaimsJws(token)
+				.getBody();
+		System.out.println(claims.get("StaffId"));
+		return (Integer) claims.get("StaffId");
+
 	}
 
 }
