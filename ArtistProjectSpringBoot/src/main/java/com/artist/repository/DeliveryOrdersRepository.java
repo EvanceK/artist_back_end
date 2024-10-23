dfb84564209a5cdf0b901d578e7bc170a4fc3ddc
Chenchen <evachen0812@gmail.com>
2024-10-23 11:18:16 +0800

    up

--- ArtistProjectSpringBoot/src/main/java/com/artist/repository/DeliveryOrdersRepository.java
+++ ArtistProjectSpringBoot/src/main/java/com/artist/repository/DeliveryOrdersRepository.java
@@ -2,37 +2,33 @@ package com.artist.repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.artist.entity.DeliveryOrders;
+import com.artist.entity.Deliveryorders;
 
-public interface DeliveryOrdersRepository extends JpaRepository<DeliveryOrders, String>{
-
+public interface DeliveryOrderRepository extends JpaRepository<Deliveryorders, String> {
+    
+  
     
     // 根據配送單查詢訂單
-    @Query("SELECT d FROM DeliveryOrders d WHERE d.deliveryNumber = :deliveryNumber")
-    List<DeliveryOrders> findByDeliveryNumber(@Param("deliveryNumber") String deliveryNumber);
-
-    // 查詢某段時間內的配送訂單
-    List<DeliveryOrders> findByCreateDateBetween(LocalDateTime start, LocalDateTime end);
-
+    @Query("SELECT d FROM Deliveryorders d WHERE d.deliveryNumber = :deliveryNumber")
+    List<Deliveryorders> findByDeliveryNumber(@Param("deliveryNumber") String deliveryNumber);
     
-    @Query("SELECT d FROM DeliveryOrders d JOIN d.orders o ON d.deliveryNumber = o.deliveryNumber")
-    List<DeliveryOrders> findAllWithOrders();
+    //用狀態去查哪張deliveryorders還沒處理
 
-
-//    // 查詢最新的配送訂單
-    @Query("SELECT d FROM DeliveryOrders d ORDER BY d.createDate DESC")
-    List<DeliveryOrders> findLatestDeliveryOrders();
 
     // 自定義查詢範例: 查詢某個配送員處理的所有配送訂單
-    @Query("SELECT d FROM DeliveryOrders d WHERE d.deliveryStaff = :staffId")
-    List<DeliveryOrders> findByDeliveryStaff(@Param("staffId") Integer staffId);
+    @Query("SELECT d FROM Deliveryorders d WHERE d.deliveryStaff = :staffId")
+    List<Deliveryorders> findByDeliveryStaff(@Param("staffId") Integer staffId);
 
     // 自定義查詢範例: 查詢是誰包裝的
-    @Query("SELECT d FROM DeliveryOrders d WHERE d.packageStaff = :staffId")
-    List<DeliveryOrders> findByPackageStaff(@Param("staffId") Integer staffId);
+    @Query("SELECT d FROM Deliveryorders d WHERE d.packageStaff = :staffId")
+    List<Deliveryorders> findByPackageStaff(@Param("staffId") Integer staffId);
 }
+
+
+//除了顯示orders還要顯示orderdetails
