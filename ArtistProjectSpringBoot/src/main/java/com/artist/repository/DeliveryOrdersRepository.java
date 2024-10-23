624c0d5317527e2a396fac1c07b6da25bcc07c86
EvanceK <155881420+EvanceK@users.noreply.github.com>
2024-10-23 16:00:32 +0800

    update again

--- ArtistProjectSpringBoot/src/main/java/com/artist/repository/DeliveryOrdersRepository.java
+++ ArtistProjectSpringBoot/src/main/java/com/artist/repository/DeliveryOrdersRepository.java
@@ -1,6 +1,5 @@
 package com.artist.repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,9 +13,13 @@ import com.artist.entity.DeliveryOrders;
 
 public interface DeliveryOrdersRepository extends JpaRepository<DeliveryOrders, String> {
     
-    @Query("SELECT d FROM DeliveryOrders d JOIN d.orders o ON d.deliveryNumber = o.deliveryNumber")
-    List<DeliveryOrders> findAllWithOrders();
-    
+	
+	@Query("SELECT d FROM DeliveryOrders d " +
+		       "JOIN d.orders o " +
+		       "JOIN o.orderDetail od")
+	List<DeliveryOrders> findAllWithOrdersAndDetails();
+	
+	
     // 根據配送單查詢訂單
 	@Query("SELECT d FROM DeliveryOrders d " +
 		       "JOIN d.orders o " +
@@ -36,10 +39,10 @@ public interface DeliveryOrdersRepository extends JpaRepository<DeliveryOrders, 
     @Query("SELECT d FROM DeliveryOrders d WHERE d.deliveryStaff = :staffId")
     List<DeliveryOrders> findByDeliveryStaff(@Param("staffId") Integer staffId);
 
+    
     // 自定義查詢範例: 查詢是誰包裝的
     @Query("SELECT d FROM DeliveryOrders d WHERE d.packageStaff = :staffId")
     List<DeliveryOrders> findByPackageStaff(@Param("staffId") Integer staffId);
 }
 
 
-//除了顯示orders還要顯示orderdetails
--- ArtistProjectSpringBoot/src/main/java/com/artist/service/impl/DeliveryOrdersServiceImpl.java
+++ ArtistProjectSpringBoot/src/main/java/com/artist/service/impl/DeliveryOrdersServiceImpl.java
@@ -97,14 +97,14 @@ public class DeliveryOrdersServiceImpl implements DeliveryOrdersService {
 			DeliveryOrders dos = Dorders.get();
 			dos.setAttName(DOrdersfDTO.getAttName());
 			dos.setAttPhone(DOrdersfDTO.getAttPhone());
-//			dos.setCreateDate(DOrdersfDTO.getCreateDate());
+			dos.setCreateDate(DOrdersfDTO.getCreateDate());
 			dos.setDeliveryAddress(DOrdersfDTO.getDeliveryAddress());
-//			dos.setDeliveryFee(DOrdersfDTO.getDeliveryFee());
+			dos.setDeliveryFee(DOrdersfDTO.getDeliveryFee());
 			dos.setDeliveryInstrictions(DOrdersfDTO.getDeliveryInstrictions());
 			dos.setDeliveryStaff(DOrdersfDTO.getDeliveryStaff());
 			dos.setPackageStaff(DOrdersfDTO.getPackageStaff());
 			dos.setStatus(DOrdersfDTO.getStatus());
-//			dos.setTotalAmount(DOrdersfDTO.getTotalAmount());
+			dos.setTotalAmount(DOrdersfDTO.getTotalAmount());
 			dor.save(dos);
 		} else {
 			System.out.println("Artist is not find");
