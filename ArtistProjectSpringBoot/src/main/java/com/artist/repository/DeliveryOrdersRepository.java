1945bb81ebac660f3a01b8f50b51cf1b78667439
Yang <ja24082010@gmail.com>
2024-10-23 11:21:25 +0800

    update

--- ArtistProjectSpringBoot/src/main/java/com/artist/repository/DeliveryOrdersRepository.java
+++ ArtistProjectSpringBoot/src/main/java/com/artist/repository/DeliveryOrdersRepository.java
@@ -8,26 +8,29 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.artist.entity.Deliveryorders;
+import com.artist.entity.DeliveryOrders;
 
-public interface DeliveryOrderRepository extends JpaRepository<Deliveryorders, String> {
+
+
+public interface DeliveryOrdersRepository extends JpaRepository<DeliveryOrders, String> {
     
-  
+    @Query("SELECT d FROM DeliveryOrders d JOIN d.orders o ON d.deliveryNumber = o.deliveryNumber")
+    List<DeliveryOrders> findAllWithOrders();
     
     // 根據配送單查詢訂單
     @Query("SELECT d FROM Deliveryorders d WHERE d.deliveryNumber = :deliveryNumber")
-    List<Deliveryorders> findByDeliveryNumber(@Param("deliveryNumber") String deliveryNumber);
+    List<DeliveryOrders> findByDeliveryNumber(@Param("deliveryNumber") String deliveryNumber);
     
     //用狀態去查哪張deliveryorders還沒處理
 
 
     // 自定義查詢範例: 查詢某個配送員處理的所有配送訂單
     @Query("SELECT d FROM Deliveryorders d WHERE d.deliveryStaff = :staffId")
-    List<Deliveryorders> findByDeliveryStaff(@Param("staffId") Integer staffId);
+    List<DeliveryOrders> findByDeliveryStaff(@Param("staffId") Integer staffId);
 
     // 自定義查詢範例: 查詢是誰包裝的
     @Query("SELECT d FROM Deliveryorders d WHERE d.packageStaff = :staffId")
-    List<Deliveryorders> findByPackageStaff(@Param("staffId") Integer staffId);
+    List<DeliveryOrders> findByPackageStaff(@Param("staffId") Integer staffId);
 }
 
 
