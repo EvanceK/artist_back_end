e57f82911a9a88c1d5916e296df3dc4ee6b91a77
EvanceK <155881420+EvanceK@users.noreply.github.com>
2024-10-25 13:37:23 +0800

    update sql

--- ArtistProjectSpringBoot/src/main/java/com/artist/repository/BidrecordRepository.java
+++ ArtistProjectSpringBoot/src/main/java/com/artist/repository/BidrecordRepository.java
@@ -34,19 +34,20 @@ public interface BidrecordRepository extends JpaRepository<Bidrecord,Long> {
 
 	
 	@Query(value = "SELECT "
-			+ "b.painting_id, "
-			+ "b.bidder_id, "
-			+ "MAX(b.bid_time) AS latest_bid_time, "
-			+ "MAX(b.bid_amount) AS latest_bid_amount, "
-			+ "b.status, "
-			+ "p.upload_date, "
-			+ "c.`name`, "
-			+ "c.email "
-			+ "FROM bidrecord b "
-			+ "JOIN Paintings p ON b.painting_id = p.painting_id "
-			+ "JOIN customers c ON b.bidder_id = c.customer_id "
-			+ "WHERE b.status = 'In Bidding' AND p.upload_date > NOW() - INTERVAL :totalDay DAY "
-			+ "GROUP BY b.painting_id, b.bidder_id, b.status, p.upload_date, c.`name`, c.email",nativeQuery = true)
+	        + "b.painting_id, "
+	        + "b.bidder_id, "
+	        + "MAX(b.bid_time) AS latest_bid_time, "
+	        + "MAX(b.bid_amount) AS latest_bid_amount, "
+	        + "c.`name`, "
+	        + "c.email, "
+	        + "(SELECT MAX(b2.bid_amount) FROM bidrecord b2 WHERE b2.painting_id = b.painting_id) AS highest_bid_amount "
+	        + "FROM bidrecord b "
+	        + "JOIN Paintings p ON b.painting_id = p.painting_id "
+	        + "JOIN customers c ON b.bidder_id = c.customer_id "
+	        + "WHERE b.status = 'In Bidding' AND p.upload_date > NOW() - INTERVAL :totalDay DAY "
+	        + "GROUP BY b.painting_id, b.bidder_id, b.status, p.upload_date, c.`name`, c.email "
+	        + "ORDER BY latest_bid_amount DESC",
+	        nativeQuery = true)
 	List<Object[]> findBidderForFinalBidding(@Param("totalDay") int totalDay);
 
 }
